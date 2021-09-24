e4b0c15fd696c16b3ff05d75dea95e4a44f2e087
Bull6 <d.i.taigin@gmail.com>
2021-09-24 17:55:35 +0500

    Update conftest.py

--- SpaceCheck/conftest.py
+++ SpaceCheck/conftest.py
@@ -1,9 +1,14 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope="session")
 def browser():
-    driver = webdriver.Chrome()
+    #driver = webdriver.Chrome("../chromedriver")
+    options = Options()
+    options.headless = True
+    driver = webdriver.Chrome("../chromedriver", chrome_options=options)
+
     yield driver
     driver.quit()
